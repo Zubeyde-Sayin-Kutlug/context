a5b77844677e73f831e40a114ff401cc62ff7639
Zubeyde-sayin-kutlug <szubeyde@hotmail.com>
2021-05-08 11:11:05 +0200

    added dark theme and light theme

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import ThemeContext from "./context/ThemeContext";
+import Header from "./Header";
+import Main from "./MainWithFunction";
+import MainWithClass from "./MainWithClass";
+function App() {
+  const themeHook = useState("light");
+  return (
+    <ThemeContext.Provider value={themeHook}>
+      <div>
+        <Header />
+        <Main />
+        <MainWithClass />
+      </div>
+    </ThemeContext.Provider>
+  );
+}
+export default App;
